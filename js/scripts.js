729e6a12b6dcb8b7a2e166b18f1f9564b843c8db
Emanuela Stetsko <emanuelastetsko.es@gmail.com>
2023-01-17 19:13:58 +0100

    Milestone 0

---
+++ js/scripts.js
@@ -0,0 +1,56 @@
+/* 
+   OUR TEAM
+
+   STEP:
+   1. 
+   2.
+   3.
+ */
+
+
+
+// Array che conterrà i membri del team
+const team = [];
+
+
+const memberOne = {
+    name: "Wayne Barnett",
+    role: "Founder & CEO",
+    photo: "wayne-barnett-founder-ceo.jpg"
+};
+
+const memberTwo = {
+    name: "Angela Caroll",
+    role: "Chief Editor",
+    photo: "angela-caroll-chief-editor.jpg"
+};
+
+const memberThree = {
+    name: "Walter Gordon",
+    role: "Office Manager",
+    photo: "walter-gordon-office-manager.jpg"
+};
+
+const memberFour = {
+    name: "Angela Lopez",
+    role: "Founder & CEO",
+    photo: "wayne-barnett-founder-ceo.jpg"
+};
+
+const memberFive = {
+    name: "Scott Estrada",
+    role: "Founder & CEO",
+    photo: "wayne-barnett-founder-ceo.jpg"
+};
+
+const memberSix = {
+    name: "Barbara Ramos",
+    role: "Founder & CEO",
+    photo: "wayne-barnett-founder-ceo.jpg"
+};
+
+// Push dei vari membri nell'array team
+team.push(memberOne, memberTwo, memberThree, memberFour, memberFive, memberSix);
+
+console.log("team", team);
+
